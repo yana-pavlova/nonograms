8384577a035230d521d808905f1f7de855fdca55
Yana Pavlova <yn-pvlv@yandex.ru>
2025-01-23 13:37:21 +0300

    feat: add basic view layer to render game

--- src/scripts/view.js
+++ src/scripts/view.js
@@ -1,16 +1,67 @@
-import { createElement } from '../utils/utils.js';
-import { numberOfLevels, modeTypes } from '../data/constants.js';
+import { createElement, changeTheme } from '../utils/utils.js';
+import {
+  numberOfLevels,
+  modeTypes,
+  levelDifficulty,
+} from '../data/constants.js';
 
 export const elements = {
+  board: null,
   levelMenu: null,
+  levelButtons: [],
 };
 
-const drawGame = (parent) => {
+const drawGame = () => {
   const fragment = document.createDocumentFragment();
+
+  fragment.append(themeButtons());
   fragment.append(createMenu());
   fragment.append(createBoard());
 
-  parent.append(fragment);
+  document.body.classList.add('page', 'theme_light');
+  document.body.append(fragment);
+};
+
+const themeButtons = () => {
+  const themeMenu = createElement({
+    tag: 'nav',
+    classes: ['theme-menu'],
+  });
+  const lightThemeButton = createElement({
+    tag: 'button',
+    classes: ['button', 'theme-menu-button', 'theme-menu-button_type_light'],
+    text: 'Light',
+  });
+  const darkThemeButton = createElement({
+    tag: 'button',
+    classes: ['button', 'theme-menu-button', 'theme-menu-button_type_dark'],
+    text: 'Dark',
+  });
+
+  lightThemeButton.disabled = true;
+
+  [lightThemeButton, darkThemeButton].forEach((button) => {
+    button.addEventListener('click', () => {
+      [lightThemeButton, darkThemeButton].forEach((btn) => {
+        btn.classList.remove('theme-menu-button_active');
+        btn.removeAttribute('disabled');
+      });
+      if ([...button.classList].includes('theme-menu-button_type_light')) {
+        changeTheme('light');
+      } else if (
+        [...button.classList].includes('theme-menu-button_type_dark')
+      ) {
+        changeTheme('dark');
+      }
+      button.classList.add('theme-menu-button_active');
+      button.setAttribute('disabled', true);
+    });
+  });
+
+  themeMenu.append(lightThemeButton);
+  themeMenu.append(darkThemeButton);
+
+  return themeMenu;
 };
 
 const createMenu = () => {
@@ -22,31 +73,43 @@ const createMenu = () => {
       classes: ['button', 'level'],
       text: modeTypes[i],
     });
-    level.dataset.level = i;
+    level.dataset.level = levelDifficulty[i];
     if (modeTypes[i] === 'easy') {
       level.disabled = true;
       level.classList.add('active');
     }
     elements.levelMenu.append(level);
+    elements.levelButtons.push(level);
   }
 
-  elements.levelButtons = elements.levelMenu.querySelectorAll('.level');
+  elements.levelButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+      elements.levelButtons.forEach((btn) => {
+        btn.classList.remove('active');
+        btn.removeAttribute('disabled');
+      });
+      button.classList.add('active');
+      button.setAttribute('disabled', true);
+      elements.board.replaceWith(createBoard(button.dataset.level));
+    });
+  });
 
   return elements.levelMenu;
 };
 
 const createBoard = (difficulty = 5) => {
-  const board = createElement({
+  elements.board = createElement({
     tag: 'div',
     classes: ['board', `board-${difficulty}`],
   });
 
   for (let i = 0; i < difficulty ** 2; i++) {
     const cell = createElement({ tag: 'div', classes: ['cell'] });
-    board.append(cell);
+    cell.addEventListener('click', () => cell.classList.toggle('active'));
+    elements.board.append(cell);
   }
 
-  return board;
+  return elements.board;
 };
 
 export default drawGame;
